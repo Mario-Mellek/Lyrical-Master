ae74b05e1549648cafce5962e6a40ccaa0b4d6de
Mario-Mellek <mario.samy266@gmail.com>
2023-03-20 13:50:03 +0200

    Style edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import Video from '../components/Video';
-import './Home.css';
+import '../styles/Home.css';
+import Logo from '../utils/lm.png';
 
 export default function Home() {
   return (
     <>
       <header>
-        <Video />
+        <nav>
+          <div className='logo'>
+            <h1>Lyrical <span>Master</span></h1>
+          </div>
+          <div className='nav-list'>
+            <ul>
+              <li>Lorem</li>
+              <li>ipsum</li>
+              <li>dolor</li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <section className='hero'>
+        <div className='description'>
+          <h1>Lyrical<span>Master</span></h1>
+          <p>
+            Lyrical<span>Master</span> is a free service where you can find the lyrics for your favorite songs
+          </p>
+          <p>
+            Start searching and sing along your favorite songs <span>NOW</span>.
+          </p>
+          <button>Search Lyrics</button>
+        </div>
+        <img src={Logo} alt="logo image" />
+      </section>
     </>
   );
 }
