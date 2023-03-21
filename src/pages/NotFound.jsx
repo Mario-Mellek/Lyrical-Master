a8ed662f3ab3cf867fcca3616779738de455e38b
Mario-Mellek <mario.samy266@gmail.com>
2023-03-21 13:24:10 +0200

    created not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import '../styles/Home.css';
+import Logo from '../utils/lm.png';
+import { Link } from 'react-router-dom';
+import { MdOutlineWarningAmber } from 'react-icons/md';
 
-export default function NotFound() {
+
+export default function Home() {
   return (
-    <div>NotFound</div>
+    <>
+      <header>
+        <nav>
+          <div className='logo'>
+            <h1>Lyrical <span>Master</span></h1>
+          </div>
+          <div className='nav-list'>
+            <ul>
+              <li>Lorem</li>
+              <li>ipsum</li>
+              <li>dolor</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <section className='hero'>
+        <div className='description'>
+          <h1>Lyrical<span>Master</span></h1>
+          <p>
+            This is an incorrect route, Check your url or go<span><Link to='/'>Home.</Link></span>
+          </p>
+          <MdOutlineWarningAmber className="warning-icon" />
+        </div>
+        <img src={Logo} alt="logo image" />
+      </section>
+    </>
   );
 }
