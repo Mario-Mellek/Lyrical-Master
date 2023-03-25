7de8951d9e98c56ff8e815fbc53d6051223b9174
Mario-Mellek <mario.samy266@gmail.com>
2023-03-25 19:43:07 +0200

    Implemented the loader

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BarLoader } from 'react-spinners';
+import PropTypes from 'prop-types';
 
-export default function NavBar() {
+export default function NavBar({ loading }) {
+  const isLoading = loading || false;
+  NavBar.propTypes = {
+    loading: PropTypes.bool,
+  };
   return (
-    <nav>
-      <div className='logo'>
-        <Link className='home-btn' to='/'><h1>Lyrical<span>Master</span></h1></Link>
-      </div>
-      <div className='nav-list'>
-        <ul>
-          <li>lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav>
+        <div className='logo'>
+          <Link className='home-btn' to='/'><h1>Lyrical<span>Master</span></h1></Link>
+        </div>
+        <div className='nav-list'>
+          <ul>
+            <li>lorem</li>
+            <li>ipsum</li>
+            <li>dolor</li>
+          </ul>
+        </div>
+      </nav>
+      <BarLoader
+        color={'rgb(224, 0, 0)'}
+        loading={isLoading}
+        cssOverride={
+          {
+            // display: 'block',
+            margin: '0 auto',
+            width: '90vw',
+            background: 'transparent',
+            borderBottomLeftRadius: '1rem',
+            borderBottomRightRadius: '1rem'
+          }
+        }
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        speedMultiplier={1}
+      />
+    </>
   );
 }
