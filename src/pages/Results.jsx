bd8d91a17d24dad8c3fedaa7d278b839628a4e02
Mario-Mellek <mario.samy266@gmail.com>
2023-04-11 22:00:41 +0200

    Netlify Deployed

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -55,7 +55,6 @@ export default function Results() {
       setIsLoading(true);
       const response = await axios.request(options);
       setSearchResults((prev) => [...prev, ...response.data.hits]);
-      console.log(response);
       setIsLoading(false);
       if (!response.data.hits.length && !searchResults.length) {
         toast.error('No results found', {
